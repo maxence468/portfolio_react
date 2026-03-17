23488692e977614f7d2c096ddbe2c11334112033
maxence468 <maxencebarth12@gmail.com>
2026-03-17 15:56:17 +0100

    modif fichier pour npm build

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import AsciiText from './component/ASCIIText'
 
 
 function App() {
-  return (
-    <>
-      <AsciiText
-        text="Maxence"
-        font="Big"
-        color="#ffffff"
-      />
-    </>
-  )
+  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AsciiText from './component/ASCIIText'
 import SpotlightCard from './component/SpotlightCard'
 import TextType from './component/TextType'
 
